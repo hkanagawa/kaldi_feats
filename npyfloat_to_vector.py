f01765962f365af6a0e093f0d20afc876f813d0a
kanagawa <kanagawa@x1extreme>
2020-07-29 16:25:08 +0900

    add npyfloat_to_vector.py

---
+++ npyfloat_to_vector.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from utils import kaldi_io
+import numpy as np
+import argparse
+import sys
+
+def main_():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("npy_list", type=str)
+	ap.add_argument("vector_wspecifier", type=str)
+
+	args = ap.parse_args()
+
+	npy_fp = open(args.npy_list, mode='rt') if args.npy_list != "-" else sys.stdin
+
+	with kaldi_io.open_or_fd(args.vector_wspecifier, "wb") as vec_writer:
+		for line in npy_fp:
+			s = line.strip().replace("\t", " ").split(" ")
+			assert len(s) == 2
+			utt, npy_file = s
+			data = np.load(npy_file)
+			assert len(data.shape) == 1
+			kaldi_io.write_vec_flt(vec_writer, data, key=utt)
+		npy_fp.close()
+
+if __name__ == '__main__':
+	main_()
